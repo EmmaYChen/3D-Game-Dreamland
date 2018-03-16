60034ad3bf69fcee691f9d5789e283c7c608f2f8
adazoom <zaida09@mail.ru>
2018-03-16 01:36:16 -0700

    Added dying animation

--- 0309_Jane updated/Assets/script/respawn.cs
+++ 0309_Jane updated/Assets/script/respawn.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class respawn: MonoBehaviour 
+
 {
+	Animator anim;
 	[SerializeField] public Transform player;
 	[SerializeField] public Transform camera;
 	[SerializeField] public Transform respawnPoint;
 
-	void OnTriggerEnter(Collider other)
+	IEnumerator OnTriggerEnter(Collider other)
 	{
-		Debug.Log ("Collides with " + other.gameObject.name);
 		if (other.gameObject.name == "shadow_rigged") {
+			player.GetComponent<Animator>().SetTrigger("death");
+			yield return new WaitForSeconds(2);
 			player.transform.position = respawnPoint.transform.position;
 			camera.transform.position = new Vector3 (75f, 2.9f, 25f);
 		}
 	}
+
 }
